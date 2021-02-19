55e84f02c69cf273701a4adcc615f6ad592d4036
Leon Ramiro <racramiro@gmail.com>
2021-02-19 11:24:09 -0500

    [update] responsive, animaciones, [add] menu

--- js/main.js
+++ js/main.js
@@ -1,17 +1,42 @@
-// window.addEventListener("scroll", function () {
-//   let animacion = this.document.getElementById("animation");
-//   let position = animacion.getBoundingClientRect().top;
-//   console.log(position);
-//   let pantalla = window.innerHeight;
-//   console.log(pantalla);
+window.addEventListener("scroll", function () {
+  let venir = this.document.getElementById("venir");
+  let position = venir.getBoundingClientRect().top;
 
-//   if (position < pantalla) {
-//     // animacion.style.animation = "mover 3s ease-out";
-//     gsap.to("#animation", {
-//       duration: 2,
-//       scale: 1,
-//       borderRadius: "50%",
-//       ease: "bounce",
-//     });
-//   }
+  let informacion = this.document.getElementById("informacion");
+  let posiinformacion = informacion.getBoundingClientRect().top;
+
+  let preguntas = this.document.getElementById("preguntas");
+  let posipreguntas = preguntas.getBoundingClientRect().top;
+
+  let masinfo = this.document.getElementById("masinfo");
+  let posimasinfo = masinfo.getBoundingClientRect().top;
+
+  let pantalla = window.innerHeight;
+
+  if (position < pantalla) {
+    venir.style.animation = "escala 1s ease-out";
+  }
+  if (posiinformacion < pantalla) {
+    informacion.style.animation = "escala 1s ease-out";
+  }
+  if (posipreguntas < pantalla) {
+    preguntas.style.animation = "escala 1s ease-out";
+  }
+  if (posimasinfo < pantalla) {
+    masinfo.style.animation = "escala 1s ease-out";
+  }
+});
+
+// console.log("hola");
+// const isIntersecting = (entry) => {
+//   return entry.isIntersecting;
+// };
+
+// const observer = new IntersectionObserver((entries) => {
+//   entries.filter(isIntersecting).forEach(accion);
 // });
+
+$("#menu").hide();
+$("#menu-btn").click(function () {
+  $("#menu").slideToggle();
+});
